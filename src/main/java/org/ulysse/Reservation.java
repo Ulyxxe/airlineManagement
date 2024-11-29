bb6b3512ed06cff8a1bc0a23e0b4d428d66bb8e6
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2024-11-29 17:04:17 +0100

    class Reservation

--- src/main/java/org/ulysse/Reservation.java
+++ src/main/java/org/ulysse/Reservation.java
@@ -1,4 +1,35 @@
 package org.ulysse;
 
+import java.time.LocalDateTime;
+
 public class Reservation {
+    private int numeroReservation;
+    private LocalDateTime dateReservation;
+    private String status;
+
+    public Reservation(int numeroReservation){
+        this.numeroReservation = numeroReservation;
+        this.dateReservation = LocalDateTime.now();
+        this.status = "unconfirmed";
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getStatus() {
+        return status;
+    }
+
+
+    public LocalDateTime getDateReservation() {
+        return dateReservation;
+    }
+
+    public int getReservation(){
+        return numeroReservation;
+    }
 }
+
+
+
+
