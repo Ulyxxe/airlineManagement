4aeafdaf8b205b2d7a91e9ef669efb8c1ac6c230
Ulyxxe <116708941+Ulyxxe@users.noreply.github.com>
2024-12-06 23:30:40 +0100

    reserver et annuler un vol

--- src/main/java/org/ulysse/Reservation.java
+++ src/main/java/org/ulysse/Reservation.java
@@ -1,16 +1,20 @@
 package org.ulysse;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Reservation {
     private int numeroReservation;
     private LocalDateTime dateReservation;
     private String status;
+    private List<Vol> volreservees;
 
     public Reservation(int numeroReservation){
         this.numeroReservation = numeroReservation;
         this.dateReservation = LocalDateTime.now();
         this.status = "unconfirmed";
+        this.volreservees = new ArrayList<>();
     }
 
     public void setStatus(String status) {
@@ -27,6 +31,13 @@ public class Reservation {
 
     public int getReservation(){
         return numeroReservation;
+    }
+
+    public void bookVol(Vol vol){
+        volreservees.add(vol);
+    }
+    public void annulerReservation(Reservation reservation ){
+
     }
 }
 
